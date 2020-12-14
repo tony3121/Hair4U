4ebbbeb22a8d6c64208d967b54f46d61017263a4
tony3121 <anthony.oloruntoba@gmail.com>
2020-12-14 11:51:19 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,22 +1,5 @@
 
 
-  $(".firstQ").on("click", function() {
-    $(".numOne").addClass("hideQ");
-    $(".numTwo").addClass("showQ");
-  })
-  $(".secQ").on("click", function() {
-    $(".numTwo").removeClass("showQ");
-    $(".numThree").addClass("showQ");
-  })
-  $(".thirdQ").on("click", function() {
-    $(".numThree").removeClass("showQ");
-    $(".numFour").addClass("showQ");
-  })
-  $(".fourthQ").on("click", function() {
-    $(".numFour").removeClass("showQ");
-    $("#submitBtn").removeClass("hideQ");
-  })
-
   $(document).ready(function() {
     $('#num2').hide();
   });
