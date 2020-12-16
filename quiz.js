80be8871db67a9596fd3ef4bbb88121a385a8e63
tony3121 <anthony.oloruntoba@gmail.com>
2020-12-16 11:47:41 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -184,6 +184,8 @@
         if (width >= 100) {
           clearInterval(id);
           i = 0;
+          
+
         } else {
           width++;
           elem.style.width = width + "%";
@@ -191,3 +193,12 @@
       }
     }
   }
+
+          	function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
